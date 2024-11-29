33cf8165848932422a2e1dbfc9f180091a544234
Nicolò Penserini - nicolo.penserini@studio.unibo.it <nicolo.penserini@studio.unibo.it>
2024-11-29 11:32:47 +0100

    Solved Ex 01

---
+++ 01-hello-world/hello.js
@@ -0,0 +1,9 @@
+console.log("Hello world!");
+
+//const tagHello = document.getElementById("ciao");
+
+const tagHello = document.querySelector("#ciao");
+tagHello.innerHTML = "Hello World";
+
+const tagYear = document.querySelector("p.anno");
+tagYear.innerHTML = "2021";
