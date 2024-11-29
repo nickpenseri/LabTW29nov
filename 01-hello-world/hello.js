6fb6ce7f2f119fc0a9c61181d4649d7095826600
Nicolò Penserini - nicolo.penserini@studio.unibo.it <nicolo.penserini@studio.unibo.it>
2024-11-29 11:37:47 +0100

    Added comments in Ex 01

--- 01-hello-world/hello.js
+++ 01-hello-world/hello.js
@@ -5,5 +5,8 @@ console.log("Hello world!");
 const tagHello = document.querySelector("#ciao");
 tagHello.innerHTML = "Hello World";
 
+
+//const tagYear = document.getElementsByClassName("anno")[0]
+//const tagYear = document.querySelectorAll("p.anno")[0];
 const tagYear = document.querySelector("p.anno");
-tagYear.innerHTML = "2021";
+tagYear.innerText = "2021";
